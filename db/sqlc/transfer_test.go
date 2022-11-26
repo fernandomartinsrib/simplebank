66bd8db6f8f484e5a0aed0252727e2050caed25d
fernandomr <fernandomartinsrib@gmail.com>
2022-11-26 19:59:16 +0100

    Created tests for transfers table

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"context"
+	"simplebank/utils"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	args := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        utils.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, args.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, args.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, args.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer := createRandomTransfer(t, account1, account2)
+
+	transfer_t, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.ID, transfer_t.ID)
+	require.Equal(t, transfer.FromAccountID, transfer_t.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, transfer_t.ToAccountID)
+	require.Equal(t, transfer.Amount, transfer_t.Amount)
+}
+
+func TestListTransfers(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+	}
+
+	args := ListTransfersParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Limit:         6,
+		Offset:        4,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), args)
+
+	require.NoError(t, err)
+	require.Len(t, transfers, 6)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.Equal(t, transfer.FromAccountID, args.FromAccountID)
+		require.Equal(t, transfer.ToAccountID, args.ToAccountID)
+	}
+}
